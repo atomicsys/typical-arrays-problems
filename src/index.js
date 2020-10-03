57fd4a7ee982620bd4ad9c631a7418f039454b56
Alex Buranok <atomicsys@yandex.ru>
2020-10-03 22:51:05 +0300

    feat: add min, max, avg algorithms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === "undefined" || array === undefined || array === [] || array.length === 0) {
+    return 0;
+  } else {
+    let result = array[0];
+    array.forEach(num => {
+      if (result > num) {
+        result = num;
+      }
+    });
+    return result;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === "undefined" || array === undefined || array === [] || array.length === 0) {
+    return 0;
+  } else {
+    let result = array[0];
+    array.forEach(num => {
+      if (result < num) {
+        result = num;
+      }
+    });
+    return result;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === "undefined" || array === undefined || array === [] || array.length === 0) {
+    return 0;
+  } else {
+    // let result = array[0];
+    let sum = 0;
+    let delim = 1;
+    array.forEach(num => {
+      sum += num;
+      result = sum / delim;
+      delim++;
+    });
+    return result;
+  }
 }
